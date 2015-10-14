1d66474b2ce0aca55a4de268fc715e8f36f5e6d6
essxiv <essxiv@gmail.com>
2015-10-14 15:21:14 -0700

    Finished login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
         flash[:success] = "Successfully Created Account"
         redirect_to @user
     else
